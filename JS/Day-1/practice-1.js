6803e815d016f7abe01a134d001675c9e96d0c60
bhagyashree <bhagyashreejadeja14@gmail.com>
2026-05-05 23:26:38 +0530

    first commit

---
+++ JS/Day-1/practice-1.js
@@ -0,0 +1,54 @@
+//The code given below is practice code for day 1 of JS
+
+//Using console.log you can see the output in the terminal.
+console.log(10);  //Here the answer will be 10
+console.log("Fresh Start");
+
+{
+    name = "John";
+    age = 21;
+}
+
+console.log(name);
+console.log(age);
+let demo = false;
+console.log(demo);
+demo = true;
+console.log(demo);
+type_of = typeof demo;
+ 
+console.log(type_of);
+
+let deviName = "Devi";
+
+console.log(deviName);
+
+deviName = "Aradhana";
+console.log(deviName);
+
+//Concatination of string
+
+let firstStr = "Hello";
+
+let secondStr = "World";
+
+let fullStr = firstStr + " " + secondStr;
+
+console.log(fullStr);
+
+//Append String
+
+let appendstr = "Hello";
+
+appendstr += " World";
+
+console.log(appendstr);
+
+//Concatinate String
+
+let str1 = "hello";
+
+let str2 = "world";
+
+let greeting = str1.concat(' ', str2);
+console.log(greeting);
