637d2ae37e45ec2e9021df390d0dd3fdcc77782b
bhagyashree <bhagyashreejadeja14@gmail.com>
2026-05-07 23:42:13 +0530

    In this file, there is code for dynamically typed language js.

---
+++ JS/Day-3/practice.js
@@ -0,0 +1,26 @@
+//The code given below is going to show typeof operator in js.
+
+//The js is dynamically typed, meaning that we don't have to specify the type of variable during declaration, like c++
+
+//Lets create some variables and check if they change there type after we ressaign them.
+
+//Also the type of operator will output the result in string
+
+let aVar = 10;
+
+console.log(aVar);
+
+console.log(typeof aVar); //This is numeric
+
+aVar = "Hello"; //This is now string
+
+console.log(aVar);
+
+console.log(typeof aVar); //This is string
+
+aVar = true; //This is now boolean
+
+console.log(aVar);
+
+console.log(typeof aVar); //This is boolean
+
